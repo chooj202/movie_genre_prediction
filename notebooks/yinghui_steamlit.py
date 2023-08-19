272c71b4fa501769c77cbca08a3e46f6e6233c6b
Yinghui Ng <yinghui.ng15@gmail.com>
2023-08-19 16:14:34 +0900

    link with API

--- notebooks/yinghui_steamlit.py
+++ notebooks/yinghui_steamlit.py
@@ -13,22 +13,7 @@ from transformers import pipeline
 import numpy as np
 
 
-
-
-#Load Models for Image Detection
-model_image = pickle.load(open('image_reg.pkl','rb'))
-
-#Preprocess Image
-def preprocess_image(image):
-    image = image.resize((256, 256))
-    image_np = np.array(image)
-    return image_np
-
-#Load Models for both
-model_both = pickle.load(open('both.pkl','rb'))
-
-
-api_url = ""
+api_url_image = "https://movie-genre-prediction-osp24vwspq-an.a.run.app/image_predict/"
 
 # def load_image_model():
 #     model = "some model"
@@ -47,9 +32,8 @@ with col1:
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image)
-        processed_uploaded_file = preprocess_image(image)
-        # response = requests.get(api_url, params=image)
-        # genre_result = response.json()
+        response = requests.get(api_url_image, params=image)
+        genre_result = response.json()["prediction"]
 
     st.header("Your Sypnosis")
     txt = st.text_area("Enter your sypnosis")
@@ -57,49 +41,17 @@ with col1:
     if sypnosis_button:
         # params = txt
         st.write(txt)
-        # result = text_classification(txt)
-
-        # #Preprocess NLP
-        # tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        # labels = ['Action',
-        #             'Adventure',
-        #             'Comedy',
-        #             'Crime',
-        #             'Fantasy',
-        #             'Horror',
-        #             'Mystery',
-        #             'Romance',
-        #             'Sci-Fi',
-        #             'Thriller']
-        # id2label = {idx:label for idx, label in enumerate(labels)}
-
-        # encoding = tokenizer(txt, return_tensors="pt")
-        # encoding = {k: v.to(model_nlp.model.device) for k,v in encoding.items()}
-        # outputs = model_nlp.model(**encoding)
-        # logits = outputs.logits
-        # sigmoid = torch.nn.Sigmoid()
-        # probs = sigmoid(logits.squeeze().cpu())
-        # predictions = np.zeros(probs.shape)
-        # predictions[np.where(probs >= 0.5)] = 1
-        # # turn predicted id's into actual label names
-        # predicted_labels = [id2label[idx] for idx, label in enumerate(predictions) if label == 1.0]
-
-        # response = requests.get(api_url, params=image)
-        # genre_result = response.json()
 
 
 with col2:
     if uploaded_file is not None:
         st.header("The Movie Genre is...")
-        st.code(model_image.predict(processed_uploaded_file))
-        st.write(f"genre_result from image")
+        st.write(f"{genre_result}")
         st.balloons()
     # else:
     #     st.write("Please upload an image file")
     if sypnosis_button:
         st.header("The Movie Genre is...")
-        # st.write(f"{result}")
-        # st.code(model_nlp.predict(clean_txt))
         st.write(f"genre_result from plot")
         st.balloons()
 
