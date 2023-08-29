95b42d9208bd544a13fea96ebb7b98aedd28fd21
Yinghui Ng <yinghui.ng15@gmail.com>
2023-08-29 19:24:20 +0900

    edit api

--- streamlit/yinghui_steamlit.py
+++ streamlit/yinghui_steamlit.py
@@ -61,6 +61,6 @@ with col2:
         st.write(f"not horror")
         response = requests.post(api_url, params={"sypnosis":txt}, files=files)
         genre_result = response.json()["prediction"]
-        st.write(f"genre_result")
+        st.write(genre_result)
 
         st.balloons()
